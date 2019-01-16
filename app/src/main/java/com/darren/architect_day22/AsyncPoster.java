2da20eeae4dbe82cc6bc097c38032a70872b3626
LYF <yifeng20161123@163.com>
2019-01-16 14:43:38 +0800

    Add some notes in AsyncPoster

--- app/src/main/java/com/darren/architect_day22/AsyncPoster.java
+++ app/src/main/java/com/darren/architect_day22/AsyncPoster.java
@@ -26,9 +26,29 @@ import java.util.concurrent.Executors;
  * @author Markus
  */
 class AsyncPoster implements Runnable {
+
+    //1.存放了订阅方法，订阅方法的参数类型，以及Subscriber注解的参数
     Subscription subscription;
+    //2.发送post请求是，post方法中传入的参数。
     Object event;
 
+    //3.线程池
+    /**
+     * CachedThreadPool:
+     * 1.是一个根据需要创建线程的线程池。
+     * 2.corePoolSize : 0  ——五核心线程
+     * 3.maximumPoolSize : Integer.MAX_VALUE  ——非核心线程是无界的
+     * 4.keepAliveTime : 60L  ——空闲线程等待新任务的最长时间为60秒
+     * 5.应用了阻塞队列SynchronousQueue,这是一个不储存元素的队列，每个
+     * 插入操作必须等待另一个线程的移除操作，同样任何一个移除操作都等待另一个线程的插入操作。
+     * 6.该线程的execute方法，首先会执行SynchronousQueue的offer方法来提交任务，并且询问
+     * 现称此中是否有空闲的线程正等待着执行SynchronousQueue的poll方法来移除任务。如果有则
+     * 配对成功，将任务交给这个空闲线程处理；如果没有配对成功，创建新的线程去处理任务。当一个线程在执行poll
+     * 方法，60秒之后依旧没有新的任务提交的时候，该条等待线程就会被终止
+     * 另外因为最大线程数(maximumPoolSize)是无界的，为了保证处理的速度，当提交的任务处理速度大于一定值，
+     * 就会创建新的线程去处理任。每次提交的任务都会立即有线程去处理。
+     *7.该类线程池，适合于（大量的）需要（立即）处理并且（耗时较少）的任务。
+     */
     private final static ExecutorService executorService = Executors.newCachedThreadPool();
 
     public AsyncPoster(Subscription subscription, Object event){
@@ -36,9 +56,11 @@ class AsyncPoster implements Runnable {
         this.event = event;
     }
 
+
     public static void enqueue(Subscription subscription, Object event) {
+        //通过构造方法，准备好参数
         AsyncPoster asyncPoster = new AsyncPoster(subscription,event);
-        // 用线程池
+        // 启用线程池，需要传入一个实现了Runnable接口的实现类
         executorService.execute(asyncPoster);
     }
 
